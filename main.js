7d4a77cad376bd20e7853a266f710dff68563208
Khriztian Moreno <khriztianmoreno@gmail.com>
2016-09-26 22:25:51 -0500

    Revert "[change] index path"

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 var express = require('express');
-var pug = require('pug');
 var app = express();
-
-app.set('views', `${__dirname}`);
-app.engine('html', pug.renderFile);
-app.set('view engine', 'html');
-
-app.use(express.static(`${__dirname}/public`));
-
-app.get('/', function (req, res) {
-	res.render('index');
-});
-
-app.listen(process.env.PORT || 3000, function () {
-	console.log(`Server listening on port: ${process.env.PORT || 3000}`);
-});
+app.use(express.static(__dirname + '/public'));
+console.log('Listening on port: ' + process.env.PORT);
+app.listen(process.env.PORT || 3000);
